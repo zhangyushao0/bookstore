0837eab684cd9bd047da6322761a1e00e8980bdc
Zhangyushao <2785874028@qq.com>
2023-12-26 06:42:23 +0000

    Add pagination support to BookRepository

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,7 @@ import com.example.demo.model.book.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByTitleContaining(String title);
+    Page<Book> findByTitleContaining(String title);
+
+    Page<Book> findAll(Pageable pageable);
 }
